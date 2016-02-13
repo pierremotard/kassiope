e05f233b9b10ab2f2f72c09ab38f74e4197ad3d7
Pierre Motard <pierre.motard99@gmail.com>
2016-02-13 20:06:22 +0100

    Remaniement issue et action

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,22 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).ready(function(){
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  window.sr = ScrollReveal();
+  sr.reveal('.scrollReveal');
+
+});
