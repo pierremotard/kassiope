1705ccc1a79f462faaad541b45eaa844deda6c55
Pierre Motard <pierre.motard99@gmail.com>
2016-05-07 20:00:39 +0200

    Site multilingue EN + FR
    2 layout files
    configuration in config.rb to choose which layout for which page

--- config.rb
+++ config.rb
@@ -17,7 +17,7 @@
 # page "/path/to/file.html", :layout => false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+page "fr/*", :layout => "layout_fr"
 #
 # A path which all have the same layout
 # with_layout :admin do
